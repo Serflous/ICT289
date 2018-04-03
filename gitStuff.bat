eae5288cd5d3c5f407ef0bb0359df726e601d0ff
Mat <mathewcausby@hotmail.com>
2018-04-03 18:26:52 +0800

    Test git script

---
+++ gitStuff.bat
@@ -0,0 +1,15 @@
+@echo off
+ECHO GitHub Commands
+ECHO Pulling...
+@echo on
+git pull
+@echo off
+PAUSE
+ECHO Uploading...
+SET message=Standard Commit
+SET /p message=Enter a commit message: 
+@echo off
+call git add .
+PAUSE
+call git commit -m "%message%"
+call git push origin master
--- main.c
+++ main.c
@@ -8,6 +8,5 @@ int main(int argc, char ** argv)
     InitializeGLUT(&argc, argv);
 
     glutMainLoop();
-
     return 0;
 }
