49f8fd4d15592989edf801ba393fff8311356c79
Mat <mathewcausby@hotmail.com>
2018-05-11 23:00:50 +0800

    Fixed collision

--- Collision.c
+++ Collision.c
@@ -8,10 +8,11 @@ bool isColliding(struct Ball ball, struct WallCollsion * walls, int numWalls, st
     for(int i = 0; i < numWalls; i++)
     {
         struct Point2D closestPoint;
-        float x1 = walls[i].x1 + levelPosition.x;
-        float x2 = walls[i].x2 + levelPosition.x;
-        float z1 = walls[i].z1 + levelPosition.z;
-        float z2 = walls[i].z2 + levelPosition.z;
+        struct WallCollsion wall = walls[i];
+        float x1 = wall.x1 + levelPosition.x;
+        float x2 = wall.x2 + levelPosition.x;
+        float z1 = wall.z1 + levelPosition.z;
+        float z2 = wall.z2 + levelPosition.z;
         FindClosestPointInLine(x1, z1, x2, z2, ballAbsPosition, &closestPoint);
 
         // Find distance between closest point and ball
@@ -21,9 +22,9 @@ bool isColliding(struct Ball ball, struct WallCollsion * walls, int numWalls, st
         if(distance <= BALL_RADIUS_PX)
         {
             // Colliding.
-            facing->x = walls[i].face.x;
-            facing->y = walls[i].face.y;
-            facing->z = walls[i].face.z;
+            facing->x = wall.face.x;
+            facing->y = wall.face.y;
+            facing->z = wall.face.z;
             return TRUE;
         }
     }
@@ -32,27 +33,23 @@ bool isColliding(struct Ball ball, struct WallCollsion * walls, int numWalls, st
 
 void FindClosestPointInLine(float x1, float z1, float x2, float z2, struct Point3D point, struct Point2D * closestPoint)
 {
-    float lineOneFromPointX = point.x - x1;
-    float lineOneFromPointZ = point.z - z1;
-    float lineOneFromLineTwoX = x2 - x1;
-    float lineOneFromLineTwoZ = z2 - z1;
-    float mag = lineOneFromLineTwoX * lineOneFromLineTwoX + lineOneFromLineTwoZ * lineOneFromLineTwoZ;
-    float dot = lineOneFromLineTwoX * lineOneFromPointX + lineOneFromLineTwoZ * lineOneFromPointZ;
-    float t = dot / mag;
-    if(t < 0)
+    float dx = x2 - x1;
+    float dz = z2 - z1;
+    float distance = ((point.x - x1) * dx + (point.z - z1) * dz) / (dx * dx + dz * dz);
+    if(distance < 0)
     {
         closestPoint->x = x1;
         closestPoint->y = z1;
     }
-    else if(t > 1)
+    else if(distance > 1)
     {
         closestPoint->x = x2;
         closestPoint->y = z2;
     }
     else
     {
-        closestPoint->x = x1 + lineOneFromLineTwoX * t;
-        closestPoint->y = x2 + lineOneFromLineTwoZ * t;
+        closestPoint->x = x1 + distance * dx;
+        closestPoint->y = z1 + distance * dz;
     }
 }
 
